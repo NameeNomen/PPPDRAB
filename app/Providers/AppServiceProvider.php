8b3319bb7417529a6bd4876a4b127decd56e8a69
NameeNomen <fat.lp3i33@gmail.com>
2026-06-22 18:21:52 +0700

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,33 +1,21 @@
 <?php
-
 namespace App\Providers;
-use Illuminate\Support\Facades\URL;
+
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <-- Pastikan ini di-import
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
-{
-    URL::forceScheme('http');
-    // if (env('CODESPACE_NAME')) {
-    //         URL::forceRootUrl('https://' . env('CODESPACE_NAME') . '-8000.app.github.dev');
-    //         URL::forceScheme('https');
-    //     }
-
-        if (!file_exists(public_path('storage'))) {
-        // symlink(storage_path('app/public'), public_path('storage'));
+    {
+        // Paksa HTTPS jika di server (production/Railway)
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
-    
-}
 }
